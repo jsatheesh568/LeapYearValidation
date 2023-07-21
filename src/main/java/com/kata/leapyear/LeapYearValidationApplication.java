2e40c1891a0f11036900d961504188c38d20cec1
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-07-21 09:13:25 +0530

    Feat: Add Leapyear project setup initial commit

---
+++ src/main/java/com/kata/leapyear/LeapYearValidationApplication.java
@@ -0,0 +1,13 @@
+package com.kata.leapyear;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeapYearValidationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeapYearValidationApplication.class, args);
+	}
+
+}
