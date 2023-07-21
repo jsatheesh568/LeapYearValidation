3ae4fcedc8d66cfdae19a92256a4ef7d4a027371
Satheeshkumar Subramanian <jsatheesh568@gmail.com>
2023-07-21 10:38:18 +0530

    Feat: Add controller class to validate the year value through restful endpoint

---
+++ src/main/java/com/kata/leapyear/controller/LeapYearController.java
@@ -0,0 +1,21 @@
+package com.kata.leapyear.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.kata.leapyear.service.LeapYearValidator;
+
+@RestController
+public class LeapYearController {
+
+	@GetMapping("/leapyear/{year}")
+	public String isLeapYear(@PathVariable int year) {
+		if (LeapYearValidator.isLeapYear(year)) {
+			return year + " is a leap year.";
+		} else {
+			return year + " is not a leap year.";
+		}
+	}
+
+}
